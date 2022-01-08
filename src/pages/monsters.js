d3783433142a2c46e507670721f0ea87603697bf
Othmane El Aissati <othmane.elaissati@student.ap.be>
2022-01-08 22:42:07 +0100

    data van wordpress opvragen

--- src/pages/monsters.js
+++ src/pages/monsters.js
@@ -1,12 +1,33 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const MonstersPage = () => {
+const MonstersPage = ({data: {allWpMonster: {edges}}}) => {
   return (
     <Layout pageTitle="Monsters of the dnd showroom">
-      <p>A list of monsters will be displayed here.</p>
+      {edges.map((item) => {
+        const monster = item.node.monsterMeta;
+        return <p key={item.node.monsterName}>{monster.monsterName} {monster.hitPoints} {monster.alignment}</p>
+      })}
     </Layout>
   )
 }
+
+export const query = graphql`
+query {
+    allWpMonster {
+      edges {
+        node {
+          monsterMeta {
+            monsterName
+            hitPoints
+            alignment
+          }
+        }
+      }
+    }
+  }
+
+`
 
 export default MonstersPage
